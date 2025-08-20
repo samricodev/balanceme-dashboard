86a4a544909c3d5057bd5424b07518ebdd295a59
samricodev <samuel.isaac.rico.estrada@gmail.com>
2025-08-20 12:40:33 -0600

    chore: base project

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="App">
+        <header className="App-header">
+          <img src="/balanceme.svg" className="App-logo" alt="logo" height="200" width="200" />
+          <h1>Welcome to Balanceme Dashboard</h1>
+          <p>
+            This is a simple dashboard for managing your balance.
+          </p>
+        </header>
+      </div>
+    </>
+  )
+}
+
+export default App
