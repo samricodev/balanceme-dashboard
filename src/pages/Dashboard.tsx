e367031d764869350d212fa3d8e5e5782b412610
samricodev <samuel.isaac.rico.estrada@gmail.com>
2025-08-28 17:25:58 -0600

    fix: reduce loading time in Dashboard component from 1000ms to 500ms

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -57,7 +57,7 @@ const Dashboard = () => {
     const timer = setTimeout(() => {
       setData(mockDashboardData);
       setLoading(false);
-    }, 1000);
+    }, 500);
 
     return () => clearTimeout(timer);
   }, []);
